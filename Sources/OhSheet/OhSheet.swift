e7c158abd8b379ab3cb9956689ddfbe2266c4314
Gary Tokman <yo@garytokman.com>
2025-05-24 18:58:32 -0700

    fix: updates

--- Sources/OhSheet/OhSheet.swift
+++ Sources/OhSheet/OhSheet.swift
@@ -17,30 +17,12 @@ struct SheetFullScreen: ViewModifier {
     
     func body(content: Content) -> some View {
         content.onAppear {
-            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-               let keyWindow = windowScene.windows.first(where: { $0.isKeyWindow }),
-               var topController = keyWindow.rootViewController
-            {
-                
-                var presentedControllers: [UIViewController] = []
-                
-                while let presented = topController.presentedViewController,
-                      presented is UIHostingController<AnyView>
-                {
-                    presentedControllers.append(presented)
-                    topController = presented
-                }
-                if let vc = presentedControllers.last, let sheet = vc.sheetPresentationController {
-                    guard let data = Data(base64Encoded: "d2FudHNGdWxsU2NyZWVu"),
-                            let key1 =  String(data: data, encoding: .utf8) else {
-                        return
+            if let sheet = currentSheetPresentationController() {
+                ["d2FudHNGdWxsU2NyZWVu", "YWxsb3dzSW50ZXJhY3RpdmVEaXNtaXNzV2hlbkZ1bGxTY3JlZW4="].forEach {
+                    if let data = Data(base64Encoded: $0),
+                       let key = String(data: data, encoding: .utf8) {
+                        sheet.setValue(true, forKey: key)
                     }
-                    guard let data2 = Data(base64Encoded: "YWxsb3dzSW50ZXJhY3RpdmVEaXNtaXNzV2hlbkZ1bGxTY3JlZW4="),
-                            let key2 =  String(data: data2, encoding: .utf8) else {
-                        return
-                    }
-                    sheet.setValue(true, forKey: key1)
-                    sheet.setValue(true, forKey: key2)
                 }
             }
         }
@@ -51,22 +33,8 @@ struct SheetFullScreenDetents: ViewModifier {
     let detents: Set<UISheetPresentationController.Detent>
     func body(content: Content) -> some View {
         content.onAppear {
-            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-               let keyWindow = windowScene.windows.first(where: { $0.isKeyWindow }),
-               var topController = keyWindow.rootViewController
-            {
-                
-                var presentedControllers: [UIViewController] = []
-                
-                while let presented = topController.presentedViewController,
-                      presented is UIHostingController<AnyView>
-                {
-                    presentedControllers.append(presented)
-                    topController = presented
-                }
-                if let vc = presentedControllers.last, let sheet = vc.sheetPresentationController {
-                    sheet.detents = Array(detents)
-                }
+            if let sheet = currentSheetPresentationController() {
+                sheet.detents = Array(detents)
             }
         }
     }
@@ -75,12 +43,37 @@ struct SheetFullScreenDetents: ViewModifier {
 public extension UISheetPresentationController.Detent {
     static func full() -> Self {
         guard let data = Data(base64Encoded: "X2Z1bGxEZXRlbnQ="),
-                let key1 =  String(data: data, encoding: .utf8) else {
+              let key1 =  String(data: data, encoding: .utf8) else {
             return Self.large()
         }
         guard let d = value(forKey: key1) as? Self else {
             return Self.large()
         }
         return d
+    }
+}
+
+@MainActor
+private func currentSheetPresentationController() -> UISheetPresentationController? {
+    if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+       let keyWindow = windowScene.windows.first(where: \.isKeyWindow),
+       var topController = keyWindow.rootViewController {
+        while let presented = topController.presentedViewController,
+              presented is UIHostingController<AnyView> {
+            topController = presented
+        }
+        return topController.sheetPresentationController
+    }
+    return nil
+}
+
+#Preview {
+    @Previewable @State var isPresented = false
+    Button(action: { isPresented = true }) {
+        Text("hello")
+    }
+    .sheet(isPresented: $isPresented) {
+        Text("world")
+            .sheet(with: [.medium(), .full()])
     }
 }
